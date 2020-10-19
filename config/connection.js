20c83de9b9c499e2d48231cf06d2362b78c1adfc
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2020-10-18 22:15:54 -0600

    local database connection set up and working, .env file added to hide password as environment variable

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+//require packages
+const mysql = require("mysql");
+require("dotenv").config();
+
+//setup sql connection
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.password,
+    database: "burger_db"
+});
+
+//initialize connection
+connection.connect(function(err) {
+    if (err) {
+        console.error("Error connection: " + err.stack);
+        return;
+    }
+    console.log("Connected as id " +connection.threadId);
+});
+
+//export to use for ORM
+// module.exports = connection;
