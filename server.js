464cafbd4081715a5cb89fcc4d768fe5ad406811
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2020-10-19 18:21:24 -0600

    installed nodemon, server initialized

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 //required pacakges and files
 const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+//static files to serve from public directory
+app.use(express.static("public"));
+
+
+//start server
+app.listen(PORT, function() {
+    console.log("Server listinging on: http://localhost:" + PORT);
+});
