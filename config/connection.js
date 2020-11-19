3d89ae546e87390f499a23ee9d204916d2133098
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2020-11-18 21:30:13 -0700

    added jawsDB environment variable to connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,21 +3,26 @@ const mysql = require("mysql");
 require("dotenv").config();
 
 //setup sql connection
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.password,
-    database: "burger_db"
-});
+let connection;
+if (process.env.JAWSBD_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.password,
+        database: "burger_db"
+    });
+}
 
 //initialize connection
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
         console.error("Error connection: " + err.stack);
         return;
     }
-    console.log("Connected as id " +connection.threadId);
+    console.log("Connected as id " + connection.threadId);
 });
 
 //export to use for ORM
